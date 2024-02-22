74f8ce1957d070997799e37c259d80170a86cd91
bhagwat dixit <Bhagwatdixit55@gmail.com>
2024-02-22 09:47:20 +0530

    js

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
-var a = document.querySelector("h1") 
-a.addEventListener("click", function(){
-    a.innerHTML = "Sadak pe!!!"
-    a.style.color = "red"
-    a.style.fontSize = "100px"
-    a.style.backgroundColor = "yellow"
-})
+// var a = document.querySelector("h1") 
+// a.addEventListener("offline", function(){
+//     a.innerHTML = "Sadak pe!!!"
+//     a.style.color = "red"
+//     a.style.fontSize = "100px"
+//     a.style.backgroundColor = "yellow"
+// })
 
-var b = document.querySelector("button")
-b.addEventListener("click", function(){
-    b.innerHTML = " Ab upar jo likha hai uspe click kr!!"
-    b.style.color = "blue"
-    b.style.backgroundColor = "green"
-})
+// var b = document.querySelector("button")
+// b.addEventListener("mouseout", function(){
+//     b.innerHTML = " Ab upar jo likha hai uspe click kr!!"
+//     b.style.color = "blue"
+//     b.style.backgroundColor = "green"
+// })
+
+// var home = document.getElementById("ho")
+// home.addEventListener("click", function(){
+//     home.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum fugiat molestias delectus esse sequi dignissimos quos reiciendi";
+//    home.style.color = "purple"
+//   home.style.fontSize= "100px"
+// })
+
+// var about = document.querySelector("#ab")
+// about.addEventListener("click", function(){
+//     about.innerHTML = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum fugiat molestias delectus esse sequi dignissimos quos reiciendis incidunt id laboriosam corrupti ut alias repellendus, illum ea amet assumenda sunt sit nostrum? Corrupti obcaecati quisquam aliquid enim tempora atque, amet illo quibusdam, ab, nihil rerum quas dolor id aut voluptates consequuntur."
+//   about.style.color= "green"
+// })
+
+// var images = document.querySelector("#gg")
+// images.addEventListener("click", function(){
+//    images.innerHTML =  '<img src="PXL_20220120_090025172.jpg" alt=""></img>'
+// images.style.height = "50px"
+
+// })
+
+const  apple1 = {
+  name: "raju",
+  age: 19,
+  location: "Gwalior",
+  "addToCart": "cart",
+sayHello: function(){console.log("helloooo");}
+}
+
+
+const  apple2 = {
+  name: "raju",
+  age: 19,
+  location: "Gwalior",
+  "addToCart": "cart",
+sayHello: function(){console.log("nonnono");}
+}
+
+apple1.sayHello();
+
+apple2.sayHello();
+
+const samsung  = {
+  type: "Va panel",
+  resolution: "Full HD",
+  screen: "anti glare",
+  "warrenty": "3 Years",
+}
+
+console.log(samsung.type);
+console.log(samsung.resolution);
+console.log(samsung.screen);
+console.log(samsung.warrenty)
+
